77c396559eee0d55bd75f9d6de0eee321235b1d4
Denis Kiura <kiura@hokela.co.ke>
2021-06-11 15:48:44 +0300

    initial project setup and housekeeping

---
+++ app/src/main/java/co/ke/paypay/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.ke.paypay.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.ke.paypay.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
